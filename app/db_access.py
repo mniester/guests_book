fcf38c12fd03b410f5396e15ea710fbd4c2d28c5
misza <mniester@gmail.com>
2021-07-13 09:00:57 +0200

    first db test done

--- app/db_access.py
+++ app/db_access.py
@@ -4,10 +4,20 @@ import sqlite3 as sql
 
 class DB_access:
     
+    temp = None
+    
     @staticmethod
     def __enter__():
         DB_access.cursor = sql.connect('test.db')
         return DB_access
+    
+    @staticmethod
+    def add_post(author, post_text):
+        DB_access.temp = (author, post_text)
+    
+    @staticmethod
+    def get_last_posts(nr = 1):
+        return DB_access.temp
     
     @staticmethod
     def __exit__(exc_type, exc_value, exc_traceback):
@@ -15,4 +25,14 @@ class DB_access:
         pass
 
 with DB_access() as db:
+    #db.cursor.execute('''CREATE TABLE user
+    #(id INT NOT NULL PRIMARY KEY,
+    #name TEXT NOT NULL CHECK(length(name) < 16));
+    #''')
     pass
+    #db.cursor.execute('''CREATE TABLE post
+    #(id INT NOT NULL PRIMARY KEY,
+    #post_text TEXT NOT NULL CHECK(length(post_text) < 1000),
+    #author INT NOT NULL,
+    #    FOREIGN KEY(author) REFERENCES user(id));
+    #''')
--- app/test_file.py
+++ app/test_file.py
@@ -1,7 +1,16 @@
 import sqlite3 as sql
-from db_access import DB_access
+from .db_access import DB_access
+
 
 
 def test_pytest():
     assert True
     assert not False
+
+def test_new_post():
+    with DB_access() as db:
+        author = 'Testowy'
+        post_text = 'Testowa'
+        db.add_post(author = author, post_text = post_text)
+        last_author, last_post_text = db.get_last_posts(nr = 1)
+        assert author == last_author and post_text == last_post_text
