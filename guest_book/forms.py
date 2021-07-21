a899bf666d0b8b5bea9bd7417e27d8b9a44d18fc
misza <mniester@gmail.com>
2021-07-21 16:17:38 +0200

    Added text area and placeholders

--- guest_book/forms.py
+++ guest_book/forms.py
@@ -2,6 +2,12 @@ from guest_book import app
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import Length, NumberRange
+from wtforms.widgets import TextArea
+
+
+
+max_nick_len = 20
+max_entry_len = 1000
 
 
 
@@ -10,8 +16,11 @@ class Entry(FlaskForm):
     length_message = ' musi być pomiędzy %(min)d i %(max)d'
     nick_message = 'Długość nicku' + length_message
     entry_message = 'Długość wpisu' + length_message
-    nick = StringField('Użytkownik', validators = [Length(min = 1, max = 20, message = nick_message)])
-    text = StringField('Wpis', validators = [Length(min = 1, max = 1000,  message = entry_message)])
+    nick_placeholder = f'Max. {max_nick_len} znaków' 
+    entry_placeholder = f'Max. {max_entry_len} znaków'
+    
+    nick = StringField('Użytkownik', validators = [Length(min = 1, max = max_nick_len, message = nick_message)])
+    text = StringField('Wpis', validators = [Length(min = 1, max = max_entry_len,  message = entry_message)], widget=TextArea())
     write = SubmitField('Zapisz')
 
 
@@ -19,7 +28,7 @@ class Entry(FlaskForm):
 class Query(FlaskForm):
 
     entry_message = 'Zapytanie musi być pomiędzy %(min)d i %(max)d'
-    entry = StringField('Zapytanie o wpis', validators = [Length(min = 1, max = 1000,  message = entry_message)])
+    entry = StringField('Zapytanie o wpis', validators = [Length(min = 1, max = max_entry_len,  message = entry_message)])
     ask = SubmitField('Szukaj')
 
 
