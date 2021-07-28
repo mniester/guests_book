a8fb64651cc8c47d0e8ad8df140f8a8e30a3c946
misza <mniester@gmail.com>
2021-07-28 19:48:40 +0200

    Debug - set max limit on user query

--- guest_book/forms.py
+++ guest_book/forms.py
@@ -5,14 +5,13 @@ from wtforms.validators import Length, NumberRange, ValidationError
 from wtforms.widgets import TextArea
 
 
-
-max_nick_len = 20
 max_entry_len = 1000
 
 
 
 class Entry(FlaskForm):
 
+    max_nick_len = 20
     length_message = ' musi być pomiędzy %(min)d i %(max)d'
     nick_message = 'Długość nicku' + length_message
     entry_message = 'Długość wpisu' + length_message
@@ -20,7 +19,8 @@ class Entry(FlaskForm):
     nick_placeholder = f'Max. {max_nick_len} znaków' 
     entry_placeholder = f'Max. {max_entry_len} znaków'
     
-    nick = StringField('Użytkownik', validators = [Length(min = 1, max = max_nick_len, message = nick_message)])
+    nick = StringField('Użytkownik', validators = [Length(min = 1, 
+        max = max_nick_len, message = nick_message)])
     text = StringField('Wpis', validators = [Length(min = 1, max = max_entry_len,  message = entry_message)], widget=TextArea())
     write = SubmitField('Zapisz')
     query = SubmitField('Szukaj')
