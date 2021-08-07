ebe2d9731d542ad8234477080b99112f65111a77
misza <mniester@gmail.com>
2021-08-07 12:27:56 +0200

    Added taking data from URL

--- guest_book/static/ajax.js
+++ guest_book/static/ajax.js
@@ -2,6 +2,15 @@ $(document).ready(function() {
   
   console.log( "ready!" );
   
+  // Taking data from URL - nr of entries and page
+  
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  if (queryString.length > 0) {
+    var quantity = urlParams.get('quantity');
+    var page = urlParams.get('page');
+    console.log(quantity, page)};
+  
   // Taking data from form - nr of entries and page
   
   $('#quantity_page').click(function (event) {
@@ -9,7 +18,5 @@ $(document).ready(function() {
     quantity = $("#quantity").val();
     page = $("#page").val();
     console.log(quantity, page);});
-  
-  // console.log(quantity, page);
 
 });
