da28d7161629955e77bb8e39fc5a44398a85b118
misza <mniester@gmail.com>
2021-08-17 00:30:28 +0200

    Debug: set proper entry size

--- DB_create.py
+++ DB_create.py
@@ -1,18 +1,21 @@
-from app.db_access import DB_access
+from json import load
+from guest_book.db_access import DB_access
 
+config_file = open('guest_book/config.json',)
+config = load(config_file)
 
 if __name__ == '__main__':
     with DB_access() as db:
 
         # I have saved table creation commands 
 
-        db.cursor.execute('''CREATE TABLE user
+        db.cursor.execute(f'''CREATE TABLE user
         (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL UNIQUE CHECK(length(name) > 1 AND length(name) < 16));
+        name TEXT NOT NULL UNIQUE CHECK(length(name) > 1 AND length(name) <= {config["MAX_USER_LEN"]}));
         ''')
-        db.cursor.execute('''CREATE TABLE post
+        db.cursor.execute(f'''CREATE TABLE entry
         (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        post_text TEXT NOT NULL CHECK(length(post_text) > 1 AND length(post_text) < 1000),
+        entry TEXT NOT NULL CHECK(length(entry) > 1 AND length(entry) <= {config["MAX_ENTRY_LEN"]}),
         user INTEGER NOT NULL,
         date TEXT NOT NULL CHECK (length(date) < 30),
         FOREIGN KEY(user) REFERENCES user(id));''')
